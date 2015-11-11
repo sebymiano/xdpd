23990dd6513b9ac06fe656b2e4a6dbb042989d0f
sebymiano <mianosebastiano@gmail.com>
2015-11-11 15:04:40 +0100

    Fix bug2

--- src/xdpd/drivers/fscale_l2sw/src/util/flow_entry_utils.c
+++ src/xdpd/drivers/fscale_l2sw/src/util/flow_entry_utils.c
@@ -69,7 +69,7 @@ bool add_or_update_eth_dst(vtss_ace_t* acl_entry, of1x_match_type_t type, of1x_m
 		BOOL port_list[VTSS_PORT_ARRAY_SIZE];
 
 		//Save the current value
-		memcpy(&port_list, &acl_entry.port_list, VTSS_PORT_ARRAY_SIZE * sizeof(acl_entry.port_list[0]));
+		memcpy(&port_list, &acl_entry->port_list, VTSS_PORT_ARRAY_SIZE * sizeof(port_list[0]));
 
 		if (vtss_ace_init(NULL, VTSS_ACE_TYPE_ETYPE, acl_entry) != VTSS_RC_OK) {
 			ROFL_ERR(
@@ -78,7 +78,7 @@ bool add_or_update_eth_dst(vtss_ace_t* acl_entry, of1x_match_type_t type, of1x_m
 		}
 
 		//Restore the previous port_in
-		memcpy(&acl_entry.port_list, &port_list, VTSS_PORT_ARRAY_SIZE * sizeof(port_list[0]));
+		memcpy(&acl_entry->port_list, &port_list, VTSS_PORT_ARRAY_SIZE * sizeof(port_list[0]));
 	}
 	//FIXME: Maybe here I should create the eth struct if it has been initialized with FRAME_ANY
 	memcpy(&acl_entry->frame.etype.dmac.value, &addr, 6);
