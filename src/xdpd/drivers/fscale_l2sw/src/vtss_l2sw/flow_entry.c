c1b2f0da076789558cf272822f249bd58489525c
Sebastiano Miano <mianosebastiano@gmail.com>
2016-11-14 18:30:19 +0100

    Fixed bug previous commit

--- src/xdpd/drivers/fscale_l2sw/src/vtss_l2sw/flow_entry.c
+++ src/xdpd/drivers/fscale_l2sw/src/vtss_l2sw/flow_entry.c
@@ -102,7 +102,7 @@ vtss_rc vtss_l2sw_generate_acl_entry_actions(vtss_ace_t* acl_entry, of1x_flow_en
 	}
 
 	if(of1x_entry->inst_grp.num_of_instructions == 0 && of1x_entry->matches.num_elements != 0) {
-		ROFL_DEBUG("["DRIVER_NAME"] vtss_l2sw_generate_acl_entry_actions: generating drop action\n", port);
+		ROFL_DEBUG("["DRIVER_NAME"] vtss_l2sw_generate_acl_entry_actions: generating drop action\n");
 		acl_entry->action.port_action = VTSS_ACL_PORT_ACTION_FILTER;
 	}
 
