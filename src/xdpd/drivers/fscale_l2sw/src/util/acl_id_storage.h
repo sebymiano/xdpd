68d126bd07240d9de3ae6ff443326296348fda50
Sebastiano Miano <mianosebastiano@gmail.com>
2016-11-16 19:29:11 +0100

    Added new acl_storage file

---
+++ src/xdpd/drivers/fscale_l2sw/src/util/acl_id_storage.h
@@ -0,0 +1,40 @@
+#ifndef _ACL_ID_STORAGE_
+#define _ACL_ID_STORAGE_
+
+#include <vector>
+#include <numeric>
+
+#define ACL_ID_INVALID -1
+#define ACL_MAX_ID 4096
+
+namespace xdpd {
+namespace fscale_l2sw {
+
+class acl_id_storage {
+
+private:
+	std::vector<int> acl_storage;
+
+	struct IncGenerator {
+	    int current_;
+	    IncGenerator (int start) : current_(start) {}
+	    int operator() () { return current_++; }
+	};
+
+    // Private constructor, singleton instance
+	acl_id_storage();
+	acl_id_storage(const acl_id_storage&);
+    void operator=(const acl_id_storage&);
+
+public:
+	static acl_id_storage& get_instance()
+        {
+		static acl_id_storage instance;
+		return instance;
+	}
+	int getAclID();
+	bool releaseAclID(int id);
+};
+
+}// namespace xdpd::fscale_l2sw
+}// namespace xdpd
--- src/xdpd/drivers/fscale_l2sw/src/util/acl_util.h
+++ src/xdpd/drivers/fscale_l2sw/src/util/acl_util.h
@@ -3,43 +3,6 @@
 
 #include <stdint.h>
 #include <rofl_datapath.h>
-#include <vector>
-#include <numeric>
-
-#define ACL_INVALID_ID -1
-#define ACL_MAX_ID 4096
-
-namespace xdpd {
-namespace fscale_l2sw {
-
-class acl_id_storage {
-
-private:
-	std::vector<int> acl_storage;
-
-	struct IncGenerator {
-	    int current_;
-	    IncGenerator (int start) : current_(start) {}
-	    int operator() () { return current_++; }
-	};
-
-    // Private constructor, singleton instance
-	acl_id_storage();
-	acl_id_storage(const acl_id_storage&);
-    void operator=(const acl_id_storage&);
-
-public:
-	static acl_id_storage& get_instance()
-        {
-		static acl_id_storage instance;
-		return instance;
-	}
-	int getAclID();
-	bool releaseAclID(int id);
-};
-
-}// namespace xdpd::fscale_l2sw
-}// namespace xdpd
 
 //C++ extern C
 ROFL_BEGIN_DECLS
